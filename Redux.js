b7863bcff56f65d9f6048b1d2d2a4e0db9975f40
Abhishek777777777 <abhishekmishra992016@gmail.com>
2023-08-06 16:19:24 +0530

    INCREMENTBY2 DECREMENTBY2

--- Redux.js
+++ Redux.js
@@ -6,26 +6,36 @@ const counterReducer = (state = { counter: 0 }, actions) => {
             counter: state.counter + 1
         };
     }
-    else {
+
+    if (actions.type === 'decrement') {
         return {
             counter: state.counter - 1
         }
     }
+
+    if (actions.type === 'INCREMENTBY2') {
+        return {
+            counter: state.counter + 2
+        };
+    }
+
+    if (actions.type === 'DECREMENTBY2') {
+        return {
+            counter: state.counter - 2
+        }
+    }
+
+
+    return state;
 }
 
 
 const store = redux.createStore(counterReducer);
+store.dispatch({ type: 'INCREMENTBY2' })
 store.dispatch({ type: 'increment' })
-store.dispatch({ type: 'increment' })
-store.dispatch({ type: 'increment' })
-store.dispatch({ type: 'increment' })
-store.dispatch({ type: 'increment' })
-
+store.dispatch({ type: 'DECREMENTBY2' })
 store.dispatch({ type: 'decrement' })
 store.dispatch({ type: 'decrement' })
-store.dispatch({ type: 'decrement' })
-
-
 
 console.log(store.getState());
 
